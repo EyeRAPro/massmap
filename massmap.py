ce0787711efdcffec2ca31fd412c2643904fd735
Sam Brown <samdanielbrown@gmail.com>
2015-02-02 23:24:23 +0000

    started work on web api

--- massmap.py
+++ massmap.py
@@ -1,11 +1,26 @@
 from flask import Flask
 import psycopg2
+import json
+from flask import request
 
 app = Flask(__name__)
+conn = psycopg2.connect("dbname=massmap user=sam")
+cur = conn.cursor()
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	cur.execute("select * from host;")
+	return json.dumps(cur.fetchall())
 
+@app.route("/ports")
+def port_list():
+	cur.execute("select DISTINCT port from port;")
+	return json.dumps(cur.fetchall())
+
+@app.route("/scans")
+def scan_for_ports():
+	ports = map(int,request.args.getlist('ports'))
+	cur.execute("SELECT id from port where port = any (%s)",(ports,))
+	return json.dumps(cur.fetchall())
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
